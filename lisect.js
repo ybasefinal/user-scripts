acfe52bf74253ba969300d5af727a8b3977d8f34
yuanlei <ybasefinal@163.com>
2019-05-26 01:33:40 +0800

    更新lisect.js版本号

--- lisect.js
+++ lisect.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         极安全网
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  在当前页面打开超链接
 // @author       You
 // @match        https://www.lisect.com/
